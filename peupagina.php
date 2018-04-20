b01fda445e51621653428455fefcf518379f8272
daroco <daroco1718daw2@gmail.com>
2018-04-20 20:12:06 +0200

    Commit1

---
+++ peupagina.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades 
+Plugin URI:
+ Escriu l'adreça URL completa del teu plugin dins de Github
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version:
+ 0.1
+Author:
+David ROyo
+Author URI:
+ https://github.com/darocodaw2/pluginwp2_davroy.git
+License: 
+GNU GPLv3 
+*/ 
+?>
